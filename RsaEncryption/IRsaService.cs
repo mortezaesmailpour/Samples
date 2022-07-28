0dce190a9c67e266e2bd269b0546d20e64ac6a7c
Morteza <morteza@esmailpour.net>
2022-07-28 23:13:43 +0430

    added IRsaService interface

---
+++ RsaEncryption/IRsaService.cs
@@ -0,0 +1,9 @@
+﻿namespace RsaEncryption;
+public interface IRsaService
+{
+    (string privateKey, string publicKey) Create();
+    string? Encrypte(string text, string publicKey);
+    string? Decrypte(string encrypteText, string privateKey);
+    string? Sign(string text, string privateKey);
+    bool Verify(string text, string signature, string publicKey);
+}
