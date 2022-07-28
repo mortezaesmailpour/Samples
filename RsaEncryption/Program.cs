bcd5e016711592bd7f7cab4aa89e2a6a95f18ec6
Morteza <morteza@esmailpour.net>
2022-07-29 02:33:44 +0430

    added try catch and stopWatch

--- RsaEncryption/Program.cs
+++ RsaEncryption/Program.cs
@@ -1,26 +1,39 @@
 ﻿using RsaEncryption;
 using RsaEncryption.Tools;
+using System.Diagnostics;
 
 Console.WriteLine("RSA Encryption Demo");
-ILogger logger = new ConsoleLogger();
 
-var msg = "Hello World";
-
-
-IRsaService rsaService = new RsaService();
-(string publicKey, string privateKey) = rsaService.Create();
-logger.LogInfo("publicKey : " + publicKey);
-logger.LogInfo("privateKey : " + privateKey);
-
-logger.LogDebug("Msg : " + msg);
-var encryptedMsg = rsaService.Encrypte(msg, publicKey);
-logger.LogDebug("encryptedMsg : " + encryptedMsg);
-var decryptedMsg = rsaService.Decrypte(encryptedMsg, privateKey);
-logger.LogDebug("decryptedMsg : " + decryptedMsg);
+RsaDemo("A");
 
 
-logger.LogDebug("Msg : " + msg);
-var signature = rsaService.Sign(msg, privateKey);
-logger.LogDebug("signature : " + signature);
-var verify = rsaService.Verify(msg, signature, publicKey);
-logger.LogDebug("verify : " + verify.ToString());
+void RsaDemo(string msg)
+{
+    ILogger logger = new ConsoleLogger();
+    logger.LogDebug("RsaDemo started with message = " + msg);
+    var stopWatch = Stopwatch.StartNew();
+    try
+    {
+        IRsaService rsaService = new RsaService();
+        (string publicKey, string privateKey) = rsaService.Create();
+        logger.LogInfo("publicKey : " + publicKey);
+        logger.LogInfo("privateKey : " + privateKey);
+
+        var encryptedMsg = rsaService.Encrypte(msg, publicKey);
+        logger.LogDebug("encryptedMsg : " + encryptedMsg);
+        var decryptedMsg = rsaService.Decrypte(encryptedMsg, privateKey);
+        logger.LogDebug("decryptedMsg : " + decryptedMsg);
+
+
+        var signature = rsaService.Sign(msg, privateKey);
+        logger.LogDebug("signature : " + signature);
+        var verify = rsaService.Verify(msg, signature, publicKey);
+        logger.LogDebug("verify : " + verify.ToString());
+    }
+    catch (Exception ex) { logger.LogError("something went wrong : " + ex.Message); }
+    finally
+    {
+        stopWatch.Stop();
+        logger.LogDebug("RsaDemo finished in " + stopWatch.ElapsedMilliseconds);
+    }
+}
